f863ecefc7f1fcbbb2c0a4bfb602c0be4f94542f
Aldo Parra <aldo.parra.dev@gmail.com>
2025-11-06 12:38:11 -0500

    update

--- src/domains/live/config/env.ts
+++ src/domains/live/config/env.ts
@@ -1,5 +1,6 @@
 export const env = {
-  SIGNALING_URL: import.meta.env.VITE_SIGNALING_URL || "ws://localhost:8080/ws",
+  SIGNALING_URL:
+    import.meta.env.VITE_SIGNALING_URL || "wss://localhost:8080/ws",
   STUN_SERVER:
     import.meta.env.VITE_STUN_SERVER || "stun:stun.l.google.com:19302",
 };
